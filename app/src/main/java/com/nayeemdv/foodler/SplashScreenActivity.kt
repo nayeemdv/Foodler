cbc2b9c8810446b2abc612e1d0e7a344497c220d
nayeem <nayeemdv@gmail.com>
2023-10-05 10:40:39 +0530

    splash screen

---
+++ app/src/main/java/com/nayeemdv/foodler/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.nayeemdv.foodler
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nayeemdv.foodler.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splashscreen.alpha = 0f
+        binding.splashscreen.animate().setDuration(2200).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
