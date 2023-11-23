a454dd348cecc249393523626514b527767662f6
nayeem <nayeemdv@gmail.com>
2023-11-23 10:38:16 +0530

    signin activity

--- app/src/main/java/com/nayeemdv/foodler/LoginActivity.kt
+++ app/src/main/java/com/nayeemdv/foodler/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.nayeemdv.foodler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nayeemdv.foodler.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDontHaveAccount.setOnClickListener {
+            startActivity(Intent(this, SignActivity::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/com/nayeemdv/foodler/SignActivity.kt
@@ -0,0 +1,21 @@
+package com.nayeemdv.foodler
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nayeemdv.foodler.databinding.ActivitySignBinding
+
+class SignActivity : AppCompatActivity() {
+    private val binding: ActivitySignBinding by lazy {
+        ActivitySignBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnHaveAccount.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
